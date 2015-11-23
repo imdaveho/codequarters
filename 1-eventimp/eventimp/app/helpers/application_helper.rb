9c4e94ba74aed90bd93c4a2ce4c65a26cc9812e9
Dave Ho <imdaveho@gmail.com>
2015-11-23 21:41:56 +0000

    railstutorial ch. 5; added layout snippets, understand naming routes and assert_select testing

--- 1-eventimp/eventimp/app/helpers/application_helper.rb
+++ 1-eventimp/eventimp/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+      base_title = "EventImp"
+      if page_title.empty?
+        base_title + " - A Social Calendar"
+      else
+        page_title + " | " + base_title + " - " + page_title
+      end
+   end
+
 end
--- 1-eventimp/eventimp/test/controllers/static_pages_controller_test.rb
+++ 1-eventimp/eventimp/test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | EventImp - Home"
+    assert_select "title", "EventImp - A Social Calendar"
   end
 
   test "should get help" do
@@ -17,5 +17,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | EventImp - About"
+  end
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | EventImp - Contact"
   end
 end
